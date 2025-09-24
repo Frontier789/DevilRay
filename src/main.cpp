a479a205d7b6247f2583364bc6dabecef291a62a
matyas-komaromi <matyas.komaromi@leica-geosystems.com>
2025-09-24 18:32:52 +0200

    Upload texture image

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,9 @@ void runEventLoop(Application &app, DrawCallback drawCallback)
 }
 
 const char* vertexShaderSource = R"glsl(
-#version 330 core
+#version 430 core
+
+out vec2 tex_coord;
 
 void main() {
     const vec2 vertices[4] = vec2[4](
@@ -70,17 +72,25 @@ void main() {
         vec2( 1.0, -1.0),
         vec2( 1.0,  1.0)
     );
+
+    tex_coord = vertices[gl_VertexID]/2.0 + 0.5;
+    
     gl_Position = vec4(vertices[gl_VertexID], 0.0, 1.0);
 }
 )glsl";
 
 const char* fragmentShaderSource = R"glsl(
-#version 330 core
+#version 430 core
+
+in vec2 tex_coord;
+
+layout(location = 0)
+uniform sampler2D tex;
 
 out vec4 frag_color;
 
 void main() {
-    frag_color = vec4(0.0, 0.0, 1.0, 1.0);
+    frag_color = texture(tex, tex_coord);
 }
 )glsl";
 
@@ -99,22 +109,22 @@ void setTextureData(GLuint texture)
     std::vector<uint32_t> pixels(800*600);
 
     auto packPixel = [](float r, float g, float b, float a) -> uint32_t {
-        return (static_cast<uint32_t>(std::clamp(r, 0.0f, 1.0f) * 255)<<24) | 
-               (static_cast<uint32_t>(std::clamp(g, 0.0f, 1.0f) * 255)<<16) | 
-               (static_cast<uint32_t>(std::clamp(b, 0.0f, 1.0f) * 255)<< 8) | 
-               (static_cast<uint32_t>(std::clamp(a, 0.0f, 1.0f) * 255)<< 0);
+        return (static_cast<uint32_t>(std::clamp(r, 0.0f, 1.0f) * 255)<< 0) | 
+               (static_cast<uint32_t>(std::clamp(g, 0.0f, 1.0f) * 255)<< 8) | 
+               (static_cast<uint32_t>(std::clamp(b, 0.0f, 1.0f) * 255)<<16) | 
+               (static_cast<uint32_t>(std::clamp(a, 0.0f, 1.0f) * 255)<<24);
     };
 
     auto setPixel = [&](int x, int y, float r, float g, float b, float a=1.0f) {
-        pixels[x + y * 600] = packPixel(r,g,b,a);
+        pixels[x + y*800] = packPixel(r,g,b,a);
     };
 
     for (int y=0;y<600;++y)
     for (int x=0;x<800;++x)
     {
-        float intensityX = std::sin(x / 30.0f * 2 * std::numbers::pi_v<float>)/2 + 0.5;
-        
-        setPixel(x, y, intensityX, intensityX, intensityX);
+        float intensity = std::sin(y / 30.0f * 2 * std::numbers::pi_v<float>)/2 + 0.5;
+    
+        setPixel(x, y, intensity, intensity, intensity);
     }
 
     glTextureSubImage2D(texture, 0, 0, 0, 800, 600, GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
@@ -142,6 +152,10 @@ int main() {
             
             glBindVertexArray(vao);
             glUseProgram(shader);
+            
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, texture);
+
             glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     
         });
